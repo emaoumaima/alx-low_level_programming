bf97fbe56e4a66c191b7875da3e57c19c3e1cfb3
emaoumaima <sellouane.oumaima@gmail.com>
2023-02-21 13:53:44 -0800

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: int a
+ * @b: int b
+ * Description: adds two integers
+ * Return: result of a+b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
