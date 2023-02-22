d67f7294eaff2b025b6687b59e4d0f7e5b3dbaf3
emaoumaima <sellouane.oumaima@gmail.com>
2023-02-22 13:19:23 -0800

    task0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test the function
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
