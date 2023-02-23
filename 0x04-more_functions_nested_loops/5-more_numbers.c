61ae0dc032c61ed38dbbf5a52ca973510625b4d7
emaoumaima <sellouane.oumaima@gmail.com>
2023-02-23 13:08:13 -0800

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,26 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * more_numbers - prints numbers between 0 to 14
+ * 10 times.
+ * Return: void
  */
-int main(void)
+
+void more_numbers(void)
 {
-    more_numbers();
-    return (0);
-}
+	int i, c;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c >= 10)
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+		i++;
+	}
+}}
