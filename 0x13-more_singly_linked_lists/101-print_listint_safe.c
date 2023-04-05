2efe678bf47b7c3c048e275581b79cb9ec49f357
emaoumaima <sellouane.oumaima@gmail.com>
2023-04-04 19:50:30 -0700

    Prints a linked list.

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a linked list.
+ * @head: A pointer to the head of the linked list.
+ * Return: The number of nodes in the linked list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *tmp;
+	size_t ans = 0;
+	int diff;
+
+	if (head != NULL)
+	{
+		while (head)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			ans++;
+			tmp = head;
+			head = head->next;
+
+			diff = head - tmp;
+			if (diff >= 0 && diff <= sizeof(listint_t))
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				exit(98);
+			}
+		}
+	}
+
+	return (ans);
+}
