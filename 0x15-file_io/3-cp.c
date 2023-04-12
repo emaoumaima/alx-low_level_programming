f028d818d45aa5f10a40fc63ae54e262e6a23bdd
emaoumaima <sellouane.oumaima@gmail.com>
2023-04-12 08:58:14 -0700

    copy file contents

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,33 +8,36 @@
   * Return: Always Return On Success 0
   */
 
-int main(int argc, char *argv[])
+int main(int lo, char *ve[])
 {
-	int fd_from, fd_to, num_read, num_write;
-	mode_t file_perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
+	int draw_from, draw_to, eli, jah;
+	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
 	char buffer[BUFSIZ];
 
-	if (argc != 3)
+	if (lo != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, file_ perm);
-	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-
-	while ((num_read = read(fd_from, buffer, BUFSIZ)) > 0)
+	draw_from = open(ve[1], O_RDONLY);
+	if (draw_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ve[1]), exit(98);
+	draw_to = open(ve[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+	if (draw_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ve[2]), exit(99);
+	eli = 1;
+	while (eli)
 	{
-		num_write = write(fd_to, buffer, num_read);
-		if (num_write == -1 || num_write != num_read)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		eli = read(draw_from, buffer, BUFSIZ);
+		if (eli == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ve[1]), exit(98);
+		if (eli > 0)
+		{
+			jah = write(draw_to, buffer, eli);
+			if (jah != eli || jah == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ve[2]), exit(99);
+		}
 	}
-
-	if (num_read == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	if (close(fd_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_form), exit(100);
-	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+	if (close(draw_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", draw_from), exit(100);
+	if (close(draw_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", draw_to), exit(100);
 	return (0);
 }
